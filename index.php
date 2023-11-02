838e31e2e31a2122f2cb7cc952a798642f384b6c
sebjacques1 <73601190+sebjacques1@users.noreply.github.com>
2023-11-02 10:04:25 +0000

    index frame

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<body style="background-color:lavender;">
+
+<h2 style="font-family:verdana">Welcome</h2>
+
+<style>
+	.button1 {boarder-radius: 12px;}
+</style>
+
+
+
+
+
+
+</body>
+</html>
