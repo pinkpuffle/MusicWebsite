b0b71fddbb06a02ab208e792e581b570c7b7a03d
purplepuffle <francescajacques64@gmail.com>
2023-11-02 16:34:17 +0000

    removed sql handling

--- system.php
+++ system.php
@@ -2,39 +2,17 @@
 <body>
 
 <?php
-//connection
-$serverName = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "musicwebsite";
-// Create connection
-$conn = new mysqli($serverName, $dbUsername, $dbPassword, $dbName);
-// Check connection
-if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
-}
-
-//login
-$username = $_POST["username"];
-$password = $_POST["password"];
-$sql = "SELECT * FROM users WHERE username = '".$username."' AND password = '".$password."'";
-$result = mysqli_query($conn, $sql);
-
-if(mysqli_num_rows($result) > 0){
-    session_start();
-	$_SESSION["login"] = true;
-	$_SESSION["username"] = $_POST["username"];
-
-    echo "Hello " . $username . "!<br>";
-    echo '<a href="/MusicWebsite/music.php">View Music</a><br>';
-    echo '<a href="/MusicWebsite/profile.php">View Profile</a>';
-}
-else{
-    echo "Incorrect login";
-}
-
-$conn->close();
-
+session_start();
+$username = $_SESSION["username"];
 ?>
+
+Hello <?php echo $username;?><br>
+
+
+<a href="/MusicWebsite/music.php">View Music</a><br>
+<a href="/MusicWebsite/profile.php">View Profile</a>
+
+
+
 </body>
 </html>
