06f88732c313107d403623ccf4a04c8713c94bc3
sebjacques1 <73601190+sebjacques1@users.noreply.github.com>
2023-11-02 10:06:59 +0000

    form for index

--- index.php
+++ index.php
@@ -7,10 +7,14 @@
 	.button1 {boarder-radius: 12px;}
 </style>
 
-
-
-
-
+<form action="/tutorial4/system.php" method="post">
+	<p style="font-family:verdana">Please login.</p>
+	<label for="username" style="font-family:'Courier New'">Username:</label><br>
+	<input type="text" id="username" name="username"><br>
+	<label for="password" style="font-family:'Courier New'">Password:</label><br>
+	<input type="text" id="password" name="password"><br><br>
+	<button class="button1">Submit</button>
+</form>
 
 </body>
 </html>
