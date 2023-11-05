bd740244bb4be6dcfb1011930ef198cbb078e2d9
purplepuffle <francescajacques64@gmail.com>
2023-11-05 20:15:39 +0000

    php cleanup

--- logMusic.php
+++ logMusic.php
@@ -1,45 +1,13 @@
 <html>
 <body>
 <?php
-
-//initalise variables
 $albumID = $_POST["log"];
-//$review = "";
-$private = 1;
-session_start();
-$username = $_SESSION["username"];
-
-//connection
-$serverName = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "musicwebsite";
-// Create connection
-$conn = new mysqli($serverName, $dbUsername, $dbPassword, $dbName);
-// Check connection
-if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
-}
-//sql
-$sql = "SELECT reviewID FROM reviews WHERE username = '" . $username . "' AND albumID = '" . $albumID . "'";
-$result = mysqli_query($conn, $sql);
-
-//if review already exist, change variables to result
-if(mysqli_num_rows($result) > 0){
-    while ($row = $result->fetch_assoc()) {
-        //$review = $row['review'];
-        //$rating = $row['rating'];
-        
-    }
-}
-
-
-
-
-
+$album = $_POST["album"];
 ?>
 
 <!-- review form -->
+<h2>Review for <?php echo $album ?></h2><br>
+
 <form action="/MusicWebsite/saveReview.php" method="post">
 
     <label for="rating" style="font-family:'Courier New'">Rating:</label><br>
