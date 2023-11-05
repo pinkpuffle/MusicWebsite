2d979ab6a7352f7bffbe173c46577acbb17441ba
purplepuffle <francescajacques64@gmail.com>
2023-11-05 19:42:06 +0000

    simplify by moving sql to file

---
+++ saveReview.php
@@ -0,0 +1,39 @@
+<html>
+<body>
+
+<?php
+//connection
+$serverName = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "musicwebsite";
+// Create connection
+$conn = new mysqli($serverName, $dbUsername, $dbPassword, $dbName);
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+//sql
+$sql = "SELECT reviewID FROM reviews WHERE username = '" . $username . "' AND albumID = '" . $albumID . "'";
+$result = mysqli_query($conn, $sql);
+
+//if review already exist
+if(mysqli_num_rows($result) > 0){
+    
+}else{ //if not
+
+}
+
+
+
+?>
+
+
+
+
+
+</body>
+
+
+
+</html>
