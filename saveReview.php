644aac86baaf344f2a9d492d69cb3651d5dce35d
purplepuffle <francescajacques64@gmail.com>
2023-11-08 03:47:55 +0000

    prepared statements

--- saveReview.php
+++ saveReview.php
@@ -25,20 +25,39 @@ if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 //sql
-$sql = "SELECT reviewID FROM reviews WHERE username = '" . $username . "' AND albumID = '" . $albumID . "'";
-$result = mysqli_query($conn, $sql);
+
+//$sql = "SELECT reviewID FROM reviews WHERE username = '" . $username . "' AND albumID = '" . $albumID . "'";
+//$result = mysqli_query($conn, $sql);
+
+$stmt = $conn->prepare("SELECT reviewID FROM reviews WHERE username = ? ND albumID = ?");
+$stmt->bind_param("si",$username,$albumID);
+$stmt->execute();
+$result = $stmt->get_result();
 
 //if review already exist
 if(mysqli_num_rows($result) > 0){
-    $sql = "UPDATE reviews
-    SET review = '" . $review . "', rating = '" . $ratingValue . "', private = '" . $private . "'
-    WHERE username = '" . $username . "'and albumID = '" . $albumID . "'";
+    //$sql = "UPDATE reviews
+    //SET review = '" . $review . "', rating = '" . $ratingValue . "', private = '" . $private . "'
+    //WHERE username = '" . $username . "'and albumID = '" . $albumID . "'";
+
+    $stmt = $conn->prepare("UPDATE reviews
+    SET review = ?, rating = ?, private = ?
+    WHERE username = ? and albumID = ?");
+    $stmt->blind_param("sibsi",$review,$ratingValue,$private,$username,$albumID);
+
+
+
 }else{ //if not
-    $sql = "INSERT INTO reviews (username, albumID, review, rating, private)
-    VALUES ('" . $username . "', '" . $albumID . "', '" . $review . "', '" . $ratingValue . "', '" . $private . "')";
+
+    //$sql = "INSERT INTO reviews (username, albumID, review, rating, private)
+    //VALUES ('" . $username . "', '" . $albumID . "', '" . $review . "', '" . $ratingValue . "', '" . $private . "')";
+
+    $stmt = $conn->prepare("INSERT INTO reviews (username, albumID, review, rating, private)
+    VALUES (?, ?, ?, ?, ?)");
+    $stmt->blind_param("sisib",$username,$albumID,$review,$ratingValue,$private);
 }
 
-if($conn->query($sql) === TRUE){
+if($stmt->execute === TRUE){
 
     echo "<h2>Review saved!</h2><br>
     " . $ratingValue . "/5<br>
